42d7f0ce9f1ff9a647f39f2132d75644dfccc35b
Ashish <ashvb2010@gmail.com>
2023-02-11 07:47:43 +0530

    chore removed console.log

--- src/utils/get-breakpoints-index.ts
+++ src/utils/get-breakpoints-index.ts
@@ -7,15 +7,8 @@ export const getBreakPointIndex = (
 ) => {
   let index = 0;
 
-  console.log('WIDTH', width);
-
   _.find(Object.keys(breakpoints), (_key: keyof IBreakPoints, i) => {
     if (width <= breakpoints[_key]) {
-      console.log({
-        bkT: _key,
-        bkvalue: breakpoints[_key],
-        width: width,
-      });
       index = i;
       return true;
     }
